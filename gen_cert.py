cd1dda12154db0d7a8eb747461c90c70e2e5237f
root <root@iZ94yq6m7k1Z.(none)>
2016-10-11 16:41:27 +0800

    update certificate templates

--- gen_cert.py
+++ gen_cert.py
@@ -471,7 +471,7 @@ class CertificateGen(object):
         HEIGHT = 210  # hight in mm (A4)
 
         LEFT_INDENT = 44  # mm from the left side to write the text
-        RIGHT_INDENT = 42  # mm from the right side for the CERTIFICATE
+        RIGHT_INDENT = 45  # mm from the right side for the CERTIFICATE
 
         # Issued ..
 
